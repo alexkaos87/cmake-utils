2a6016e867442827ccb2b411210353721c4a9ad2
alexkaos87 <alessiosalvatore87@gmail.com>
2024-03-20 23:17:50 +0100

    adding single project cmake example

--- samples/SingleProject/main.cpp
+++ samples/SingleProject/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "BiggerClass.h"
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World Single Project!" << std::endl;
+
+    BiggerClass biggerClass;
+    
     return 0;
 }
